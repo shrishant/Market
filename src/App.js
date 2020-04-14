d9f1bbe450c4bb36492fc9ff8ea4b7e5749864a4
unknown <Shrishant>
2020-04-14 10:03:42 +0545

    git :qw

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { createFactory } from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+import {  Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-        <Homepage />
+    <div>
+      <Route exact path="/hats" component={Hats}/>
+      <Route exact path="/" component={Homepage} />
+    </div>
   );
 }
 
-export default App;
+
+const Hats = () => {
+  return ( 
+    <div>
+      <h1>Hats page</h1>
+    </div>
+   );
+}
+ 
+export default  App;
--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,9 +1,11 @@
 import React  from 'react';
-import './menu-item.scss'
+import { withRouter } from 'react-router-dom';
+import './menu-item.style.scss'
 
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({title,imageUrl,size, history,linkUrl,match}) => {
     return ( 
-        <div  className={ `${size} menu-item`}>
+        <div  className={ `${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div style={{      backgroundImage :`url(${imageUrl})`}}
                 className={ ` background-image` } />
         <div className="content">
@@ -14,4 +16,4 @@ const MenuItem = ({title,imageUrl,size}) => {
      );
 }  
  
-export default MenuItem;
+export default withRouter(MenuItem);
